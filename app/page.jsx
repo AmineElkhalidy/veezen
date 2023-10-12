aa39e98e97892fc123aff565dfff9c366f336eae
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2023-10-12 23:20:26 +0100

    Pushing clients and about and how it works sections...

--- app/page.jsx
+++ app/page.jsx
@@ -4,10 +4,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
+      {/* Hero */}
       <section class="bg-primaryLighter pt-[3rem] pb-[6rem] ">
-        <div class="max-w-[120rem] py-[3rem] mx-auto flex flex-col gap-40 px-[2rem] md:flex-row lg:gap-10 ">
+        <div class="max-w-[120rem] py-[3rem] mx-auto flex flex-col gap-40 px-[2rem] md:flex-row xl:px-0 lg:gap-10">
           <div class="text-center md:text-left md:w-full">
-            <h1 class="heading-primary text-[3.5rem] mb-10 lg:text-[5.2rem] lg:mb-[3.2rem] ">
+            <h1 class="heading-primary text-[3.5rem] mb-10 lg:text-[5.2rem] lg:mb-[3.2rem]">
               Mental Health & Well-being in the Workplace
             </h1>
             <p class="hero-description">
@@ -70,6 +71,139 @@ export default function Home() {
 
             <Link href="#meals" class="btn btn--primary">
               Get Started
+            </Link>
+          </div>
+
+          <div class="text-center self-center md:w-full">
+            <Image
+              src="/assets/images/home/hero.svg"
+              width={450}
+              height={450}
+              className="object-contain lg:w-[600px] "
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* Clients */}
+      <section className="pt-[2.4rem] pb-[6.4rem] lg:pt-[4.8rem]  ">
+        <div className="max-w-[120rem] py-[3rem] mx-auto px-[2rem] xl:px-0">
+          <h2 className="text-[30px] text-center font-semibold mb-20 md:mb-28">
+            Our Clients
+          </h2>
+
+          <div className="flex justify-around gap-12">
+            <img
+              src="/assets/images/home/sodexo.png"
+              alt="Sedoxo logo"
+              className="logo h-[3rem] lg:h-[5rem] "
+            />
+            <img
+              src="/assets/images/home/taqa.svg"
+              alt="TAQA logo"
+              className="logo h-[3rem] lg:h-[5rem] "
+            />
+            <img
+              src="/assets/images/home/jti.svg"
+              alt="JTI logo"
+              className="logo h-[3rem] lg:h-[5rem] "
+            />
+            <img
+              src="/assets/images/home/zalar.png"
+              alt="Zalar logo"
+              className="logo h-[3rem] lg:h-[5rem] "
+            />
+            <img
+              src="/assets/images/home/jesa.png"
+              alt="JESA logo"
+              className="logo h-[3rem] lg:h-[5rem] "
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* Services */}
+      <section className="pt-[2.4rem] pb-[6.4rem] lg:pt-[4.8rem] lg:pb-[9.6rem]">
+        <div className="max-w-[120rem] py-[3rem] mx-auto px-[2rem] xl:px-0">
+          <span class="subheading">How it works</span>
+          <h2 class="heading-secondary">How can we help you?</h2>
+        </div>
+
+        <div class="max-w-[120rem] py-[3rem] mx-auto px-[2rem] xl:px-0 grid grid--2-cols grid--center-v">
+          <div class="step-text-box">
+            <p class="step-number">01</p>
+            <h3 class="heading-tertiary">Assessment and Support Evaluation</h3>
+            <p class="step-description">
+              : In the initial stage, we conduct a comprehensive assessment of
+              the individual's mental health and well-being needs. Our expert
+              team evaluates the specific challenges and stressors they may face
+              in the workplace. This step forms the foundation for tailored
+              support strategies.
+            </p>
+          </div>
+          <div class="step-image-box">
+            <img
+              src="/assets/images/home/1.svg"
+              alt="Picture"
+              class="step-image"
+            />
+          </div>
+
+          <div class="step-image-box">
+            <img
+              src="/assets/images/home/2.svg"
+              alt="Picture"
+              class="step-image"
+            />
+          </div>
+          <div class="step-text-box">
+            <p class="step-number">02</p>
+            <h3 class="heading-tertiary">Personalized Well-being Plans</h3>
+            <p class="step-description">
+              : Based on the assessment results, we create personalized
+              well-being plans. These plans include a range of interventions,
+              such as stress management techniques, resilience-building
+              exercises, and mental health resources. Each plan is uniquely
+              designed to address the individual's requirements and promote
+              their overall mental well-being.
+            </p>
+          </div>
+
+          <div class="step-text-box">
+            <p class="step-number">03</p>
+            <h3 class="heading-tertiary">Ongoing Support and Resources</h3>
+            <p class="step-description">
+              : We understand that well-being is an ongoing journey. Our
+              services offer continuous support, including access to mental
+              health professionals, workshops, and educational resources. We aim
+              to empower individuals with the tools and knowledge they need to
+              maintain and enhance their mental health while thriving in the
+              workplace.
+            </p>
+          </div>
+          <div class="step-image-box">
+            <img
+              src="/assets/images/home/3.svg"
+              alt="Picture"
+              class="step-image"
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* About */}
+      <section class="bg-primaryLighter pt-[3rem] pb-[6rem] ">
+        <div class="max-w-[120rem] py-[3rem] mx-auto flex flex-col gap-40 px-[2rem] md:flex-row xl:px-0 lg:gap-10">
+          <div class="text-center md:text-left md:w-full">
+            <h2 class="heading-secondary mb-8">Get to know about us</h2>
+            <p class="hero-description">
+              Veezen: Shaping Workplace Well-being. We specialize in
+              transforming workplaces into nurturing, mental-health-focused
+              environments, fostering employee happiness and business success.
+            </p>
+
+            <Link href="#meals" class="btn btn--primary">
+              Learn more
             </Link>
           </div>
 
