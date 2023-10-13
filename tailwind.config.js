a60887f17382baafeff2d5d215987ed1e425663a
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2023-10-13 11:47:05 +0100

    TailwindCSS config file changed!

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // primary: "#e67e22",
         primary: "#5DC4BB",
-        // primaryDark: "#cf711f",
         primaryDark: "#387670",
-        // primaryLighten: "#f8d8bd",
         primaryLighten: "#7dd0c9",
-        // primaryLight: "#fae5d3",
         primaryLight: "#9edcd6",
-        // primaryLighter: "#fdf2e9",
         primaryLighter: "#eff9f8",
         grey: "#555",
         darkGrey: "#333",
